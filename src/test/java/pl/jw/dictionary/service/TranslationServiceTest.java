0c7c106441ac22728aa36dfe2dac4c337fac080f
jakub witkowski <jmwitkowski0@gmail.com>
2021-01-23 18:43:43 +0100

    Added first tests

--- src/test/java/pl/jw/dictionary/service/TranslationServiceTest.java
+++ src/test/java/pl/jw/dictionary/service/TranslationServiceTest.java
@@ -10,8 +10,20 @@ class TranslationServiceTest {
     private TranslationService translationService;
 
     @Test
+    void shouldTranslateWord()  {
+        String translate = translationService.translate("Testowanie");
+        assertEquals(translate, "Testing");
+    }
+
+    @Test
     void shouldTranslateSentence()  {
         String translate = translationService.translate("Testowanie jest ważne");
         assertEquals(translate, "Testing is important");
+    }
+
+    @Test
+    void shouldTranslateSentenceAndKeepPunctuation() {
+        String translate = translationService.translate("Testowanie, jest ważne!");
+        assertEquals(translate, "Testing, is important!");
     }
 }
